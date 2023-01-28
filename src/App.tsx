6b71ab6b16167b978b4aebeb30603904c21b5ff3
Tiago Teles <tteles1@ford.com>
2023-01-28 12:04:40 -0500

    Add basic tabs router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import * as React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import { Box } from '@mui/material';
+import MainTabs from './components/MainTabs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Play from './views/Play';
 
 function App() {
   return (
-    <div className="App">
-      App component
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <Box sx={ {width: '100%' } }>
+      <BrowserRouter>
+        <MainTabs />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </BrowserRouter>
+    </Box>
   );
 }
 
---
+++ src/views/Play.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+
+export default function Play() {
+    return (
+        <Box sx={{ width: '100%', minHeight: '300px', backgroundColor: 'blue' }}>
+            Play page under construction
+        </Box>
+    );
+}
